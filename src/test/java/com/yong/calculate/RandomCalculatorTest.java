33650ab63c737428cb11e8ac9be7bff47aa5e20d
ChangYong <lcy9002@naver.com>
2017-08-18 17:09:43 +0900

    mockito

--- src/test/java/com/yong/calculate/RandomCalculatorTest.java
+++ src/test/java/com/yong/calculate/RandomCalculatorTest.java
@@ -6,6 +6,8 @@ import java.util.Random;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class RandomCalculatorTest {
     @Test
@@ -17,6 +19,21 @@ public class RandomCalculatorTest {
                 return 3;
             }
         }, new Calculator());
+
+        // when
+        int result = randomCalculator.plus(2, 5);
+
+        // then
+        assertThat(result, is(10));
+    }
+
+    @Test
+    public void plus_mockito(){
+        // given
+        Random random = mock(Random.class);
+        when(random.nextInt()).
+                thenReturn(3);
+        RandomCalculator randomCalculator = new RandomCalculator(random, new Calculator());
 
         // when
         int result = randomCalculator.plus(2, 5);
