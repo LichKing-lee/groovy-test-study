bed68d18aeee21b8bcf2d7a1fbfc923f4f058e8c
ChangYong <lcy9002@naver.com>
2017-08-20 16:02:06 +0900

    groovy test case

---
+++ src/test/java/com/yong/calculate/groovy/RandomCalculatorTest.groovy
@@ -0,0 +1,51 @@
+package com.yong.calculate.groovy
+
+import com.yong.calculate.Calculator
+import com.yong.calculate.RandomCalculator
+import groovy.mock.interceptor.MockFor
+import org.junit.Test
+
+class RandomCalculatorTest {
+    @Test
+    void plus(){
+        // given
+        def randomCalculator = new RandomCalculator([nextInt: { -> 3}] as Random, new Calculator())
+
+        // when
+        def result = randomCalculator.plus(2, 5)
+
+        // then
+        assert result == 10
+    }
+
+    @Test
+    void plus_mockito(){
+        // given
+        def mock = new MockFor(Random)
+        mock.demand.nextInt{ 3 }
+        mock.use {
+            def calculator = new RandomCalculator(new Random(), new Calculator())
+
+            // when
+            def result = calculator.plus(2, 5)
+
+            // then
+            assert result == 10
+        }
+        mock.expect.verify()
+    }
+
+    @Test
+    void groovy_mocking_test(){
+        // given
+        def mock = new MockFor(Random)
+        mock.demand.nextInt{ 5 }
+        mock.use {
+            // when
+            def result = new Random().nextInt()
+
+            // then
+            assert result == 5
+        }
+    }
+}
